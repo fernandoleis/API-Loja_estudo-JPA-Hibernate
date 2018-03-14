e0f876253bdc364fad5baf97570c47d6315b56df
Fernando Leis <fernandoleis@gmail.com>
2018-03-13 23:13:12 -0300

    Criteria API do Hibernate

--- src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -20,54 +20,86 @@ import br.com.caelum.model.Produto;
 @Repository
 public class ProdutoDao {
 
-	@PersistenceContext
-	private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-	public List<Produto> getProdutos() {
-		return em.createQuery("from Produto", Produto.class).getResultList();
-	}
+    public List<Produto> getProdutos() {
+        return em.createQuery("from Produto", Produto.class).getResultList();
+    }
 
-	public Produto getProduto(Integer id) {
-		Produto produto = em.find(Produto.class, id);
-		return produto;
-	}
+    public Produto getProduto(Integer id) {
+        Produto produto = em.find(Produto.class, id);
+        return produto;
+    }
 
-	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
-		Root<Produto> root = query.from(Produto.class);
+    public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
 
-		Path<String> nomePath = root.<String> get("nome");
-		Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
-		Path<Integer> categoriaPath = root.join("categorias").<Integer> get("id");
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = query.from(Produto.class);
 
-		List<Predicate> predicates = new ArrayList<>();
+        Path<String> nomePath = root.get("nome");
+        Path<Integer> categoriaPath = root.join("categorias").get("id");
+        Path<Integer> lojaPath = root.get("loja").get("id");
 
-		if (!nome.isEmpty()) {
-			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
-			predicates.add(nomeIgual);
-		}
-		if (categoriaId != null) {
-			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
-			predicates.add(categoriaIgual);
-		}
-		if (lojaId != null) {
-			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
-			predicates.add(lojaIgual);
-		}
+        ArrayList<Predicate> predicates = new ArrayList<>();
 
-		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
+        if (!nome.isEmpty()) {
+            Predicate nomeIgual = criteriaBuilder.like(nomePath, "%" + nome + "%");
+            predicates.add(nomeIgual);
+        }
 
-		TypedQuery<Produto> typedQuery = em.createQuery(query);
-		return typedQuery.getResultList();
+        if (categoriaId != null) {
+            Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath,
+                    categoriaId);
+            predicates.add(categoriaIgual);
+        }
 
-	}
+        if (lojaId != null) {
+            Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
+            predicates.add(lojaIgual);
+        }
 
-	public void insere(Produto produto) {
-		if (produto.getId() == null)
-			em.persist(produto);
-		else
-			em.merge(produto);
-	}
+        query.where((Predicate[]) predicates.toArray(new Predicate[0]));
+        TypedQuery<Produto> typedQuery = em.createQuery(query);
+        return typedQuery.getResultList();
+
+
+//		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+//		Root<Produto> root = query.from(Produto.class);
+//
+//		Path<String> nomePath = root.<String> get("nome");
+//		Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
+//		Path<Integer> categoriaPath = root.join("categorias").<Integer> get("id");
+//
+//		List<Predicate> predicates = new ArrayList<>();
+//
+//		if (!nome.isEmpty()) {
+//			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
+//			predicates.add(nomeIgual);
+//		}
+//		if (categoriaId != null) {
+//			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
+//			predicates.add(categoriaIgual);
+//		}
+//		if (lojaId != null) {
+//			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
+//			predicates.add(lojaIgual);
+//		}
+//
+//		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
+//
+//		TypedQuery<Produto> typedQuery = em.createQuery(query);
+//		return typedQuery.getResultList();
+
+    }
+
+    public void insere(Produto produto) {
+        if (produto.getId() == null)
+            em.persist(produto);
+        else
+            em.merge(produto);
+    }
 
 }
--- src/main/java/br/com/caelum/model/Produto.java
+++ src/main/java/br/com/caelum/model/Produto.java
@@ -1,14 +1,10 @@
 
 package br.com.caelum.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
@@ -17,81 +13,93 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Entity
 public class Produto {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@NotEmpty
-	private String nome;
-	@NotEmpty
-	private String linkDaFoto;
-	
-	@NotEmpty
-	@Column(columnDefinition="TEXT")
-	private String descricao;
-	
-	@Min(20)
-	private double preco;
-	
-	
-	@Valid
-	@ManyToOne
-	private Loja loja;
-	
-	
-	public String getDescricao() {
-		return descricao;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty
+    private String nome;
+
+    @NotEmpty
+    private String linkDaFoto;
+
+    @ManyToMany
+    private List<Categoria> categorias = new ArrayList<>();
+
+    @NotEmpty
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Min(20)
+    private double preco;
+
+
+    @Valid
+    @ManyToOne
+    private Loja loja;
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    //método auxiliar para associar categorias com o produto
+    //se funcionar apos ter definido o relacionamento entre produto e categoria
+	public void adicionarCategorias(Categoria... categorias) {
+		for (Categoria categoria : categorias) {
+			this.categorias.add(categoria);
+		}
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	//método auxiliar para associar categorias com o produto
-	//se funcionar apos ter definido o relacionamento entre produto e categoria
-//	public void adicionarCategorias(Categoria... categorias) {
-//		for (Categoria categoria : categorias) {
-//			this.categorias.add(categoria);
-//		}
-//	}
+    public String getLinkDaFoto() {
+        return linkDaFoto;
+    }
 
-	public String getLinkDaFoto() {
-		return linkDaFoto;
-	}
-	
-	public double getPreco() {
-		return preco;
-	}
+    public double getPreco() {
+        return preco;
+    }
 
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 
-	public void setLinkDaFoto(String linkDaFoto) {
-		this.linkDaFoto = linkDaFoto;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setLinkDaFoto(String linkDaFoto) {
+        this.linkDaFoto = linkDaFoto;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setLoja(Loja loja) {
-		this.loja = loja;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public Loja getLoja() {
-		return loja;
-	}
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
 
+    public Loja getLoja() {
+        return loja;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
 }
